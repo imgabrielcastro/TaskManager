5c20000244fef6d64f6b4c45d5c1ff4599770fe9
Gabriel Castro <josegabrielsouzacastro@gmail.com>
2025-10-20 02:37:52 -0300

    style tasks component

--- tasks/src/components/Task.tsx
+++ tasks/src/components/Task.tsx
@@ -1,30 +1,47 @@
-import {theme} from "../themes/theme";
-import {Text} from "react-native-paper";
+import { theme } from "../themes/theme";
+import { Text } from "react-native-paper";
 import VStack from "./Stacks/VStack";
 import commonStyles from "../constants/commonStyles";
-import {StyleSheet} from "react-native";
+import { StyleSheet } from "react-native";
+import HStack from "./Stacks/HStack";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { SCREEN_WIDTH, SCREEN_HEIGHT as width } from "../constants";
 
 interface ITaskProps {
-    desc: string;
-    estimateAt: Date;
-    doneAt?: Date | null;
+  desc: string;
+  estimateAt: Date;
+  doneAt?: Date | null;
 }
 
 export default function Task({ desc, estimateAt, doneAt }: ITaskProps) {
-    return (
-        <VStack style={{
-        }}>
-            <Text variant="bodyLarge" style={styles.taskContainer}>{desc}</Text>
-            <Text variant="bodyMedium" style={styles.taskContainer}>{estimateAt.toString()}</Text>
-            {doneAt && <Text variant="bodyMedium" style={styles.taskContainer}>{doneAt.toString()}</Text>}
+  return (
+    <HStack
+      style={{
+        borderColor: theme.colors.background,
+        borderWidth: 1,
+        padding: 12,
+        alignItems: "center",
+      }}
+    >
+        <VStack style={{ width: SCREEN_WIDTH * 0.20 }}>
+          
         </VStack>
-    )
+
+      <VStack>
+      <Text variant="bodyMedium" style={styles.taskContainer}>
+        {desc}
+      </Text>
+      <Text variant="bodyMedium" style={styles.taskContainer}>
+        {estimateAt.toString()}
+      </Text>
+      </VStack>
+    </HStack>
+  );
 }
 
 const styles = StyleSheet.create({
-    taskContainer: {
-        fontFamily: commonStyles.fontFamily,
-        color: theme.colors.background,
-    },
+  taskContainer: {
+    fontFamily: commonStyles.fontFamily,
+    color: theme.colors.background,
+  },
 });
-
